cd6d652d2927e979c8f739df780f31d9b59f847b
atefehzangooei <a_zangooei90@yahoo.com>
2026-01-05 08:23:06 +0330

    social spring boot

--- src/main/kotlin/com/social/social/service/JwtService.kt
+++ src/main/kotlin/com/social/social/service/JwtService.kt
@@ -1,4 +1,11 @@
 package com.social.social.service
 
+import org.springframework.stereotype.Service
+import java.security.Key
+
+@Service
 class JwtService {
+
+    private val sercetKey : Key =
+
 }
